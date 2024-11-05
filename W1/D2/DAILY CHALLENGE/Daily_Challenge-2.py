8f994463de141aadb647751352294ce96e366133
MikelTWasTaken <mike.b.teitelbaum@gmail.com>
2024-11-05 09:33:21 +0200

    update to daily challenge

--- W1/D2/DAILY CHALLENGE/Daily_Challenge-2.py
+++ W1/D2/DAILY CHALLENGE/Daily_Challenge-2.py
@@ -23,19 +23,34 @@
 # for i in range (len(my_nums)):
 #     output += my_nums[i]
 #     print(output)
+# #attempt 1
+# number = int(input("give me a number please: ")) #input needed because we are asking someone for something and need a value in return.
+# length = int(input("how long is the list? ")) #ditto above
 
-number = int(input("give me a number please: ")) #input needed because we are asking someone for something and need a value in return.
-length = int(input("how long is the list? ")) #ditto above
+# list_of_multiples = [] #need to define a list from instructions. Multiples mean whatever number we give will be multiplied by itself. The length indicates how long that list will be.
+# for i in range (length +1): #for i in range is probably what we need here because we have a range of data that needs to be looped through
+#       list_of_multiples.append(number)
+# print(list_of_multiples)
 
-list_of_multiples = [] #need to define a list from instructions. Multiples mean whatever number we give will be multiplied by itself. The length indicates how long that list will be.
-for i in range (length +1): #for i in range is probably what we need here because we have a range of data that needs to be looped through
-      list_of_multiples.append(number)
-print(list_of_multiples)
 #I keep getting the same number repeated by the length of the list. I need to multiply something but can't figure out where.
 # EXAMPLE: 
 #give me a number please: 7
 # how long is the list? 9
 # [7, 7, 7, 7, 7, 7, 7, 7, 7, 7]
+
+# #attempt 2 after watching https://www.youtube.com/watch?v=3dt4OGnU5sM
+# list_of_multiples = [number*number for i in length]
+# length = int(input("how long is the list? "))
+# number = int(input("give me a number please: "))
+# print(list_of_multiples)
+
+#final attempt:
+number = int(input("give me a number please: "))
+length = int(input("how long is the list? "))
+
+list_of_multiples = [number * i for i in range(1, length + 1)]
+
+print(list_of_multiples)
 
 
 # Challenge 2
@@ -58,6 +73,6 @@ users_word = input("Give a word with repeating letters: ")
 new_string = ""
 #next I want to create a code where the word will run through a loop so that any duplicates are removed. for or while? for, because while needs to be proven false.
 for i in range(users_word):
-      if i == -1
-print(len(users_word))
+      if i == -1:
+        print(len(users_word))
 #This one gets me lost. I am not sure which terms, numbers, placements or orders to use. 
