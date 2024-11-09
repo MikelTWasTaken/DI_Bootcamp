dd3fec279917f6c949f3dc28f3255268f9257e6f
MikelTWasTaken <mike.b.teitelbaum@gmail.com>
2024-11-09 14:19:13 +0200

    xp update

--- W1/D3/EXERCISE XP/XP_Ninja_3.py
+++ W1/D3/EXERCISE XP/XP_Ninja_3.py
@@ -13,3 +13,24 @@
 # Print out the companies without duplicates, in a comma-separated string with no line-breaks (eg. “Acura, Alfa Romeo, Aston Martin, …”), also print out a message saying how many companies are now in the list.
 
 # Bonus: Print out the list of manufacturers in ascending order (A-Z), but reverse the letters of each manufacturer’s name.
+
+Car_comp = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet"
+build_stats = Car_comp.split(',')
+print(build_stats)
+
+for name in build_stats:
+    build_stats.sort()
+print(build_stats)
+
+build_stats_sum = len(build_stats)
+print(f'There are {build_stats_sum} car companies in this list')
+
+build_stats.sort()
+if build_stats:
+    print(build_stats)
+
+build_stats_len = [word for word in build_stats if 'o' in word]  # Filter words with 'o'
+print(f'There are {len(build_stats_len)} car companies with "O" in this list')
+
+build_stats_len = [word for word in build_stats if 'i' not in word]  
+print(f'There are {len(build_stats_len)} car companies without "I" in this list')
