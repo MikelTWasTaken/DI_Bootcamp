120d863c15e5e158612b170b877faba479e3c8c8
MikelTWasTaken <mike.b.teitelbaum@gmail.com>
2024-11-12 21:15:49 +0200

    dc day 3

--- W2/D3/DAILY CHALLENGE/DC_D3_W2.py
+++ W2/D3/DAILY CHALLENGE/DC_D3_W2.py
@@ -13,4 +13,29 @@
 # Be able to put them in a list and sort them
 # Bonus (not mandatory) : Install the Turtle module, and draw the sorted circles
 
+class Circle():
+    def __init__(self, radius):
+        self.radius = radius
+        self.diameter = radius * 2 #diamerter is the radius times 2 
 
+    def get_radius(self):
+        return self.radius 
+    def get_diameter(self):
+        return self.diameter
+    def get_area(self):
+        PI = 3.142
+        return PI *(self.radius ** 2) 
+    def __str__(self):
+        return f"Circle with radius {self.radius} and diameter {self.diameter}"
+    
+cr1 = Circle(5)
+cr2 = Circle(10)
+
+
+print(cr1) #Circle with radius 5 and diameter 10
+print(cr2) #Circle with radius 10 and diameter 20
+
+if cr1.get_area() > cr2.get_area():
+    print("Circle 1 is bigger")
+else:
+    print("Circle 2 is bigger")
